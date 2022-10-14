9466e952f52e82f009c8e216761c5ae48e2399af
ja54312 <ija54312@gmail.com>
2022-10-14 13:00:34 -0500

    jest 3

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -27,3 +27,30 @@ test('Probar un Callback',()=>{
         expect(str).toBe('aloH')
     })
 })
+
+const reverseString2 = str => {
+    return new Promise((resolve,reject)=>{
+        if(!str){
+            reject(Error('Error'))
+        }
+        resolve(str.split("").reverse().join(""))
+    })
+}
+
+test('Probar una promesa',()=>{
+    return reverseString2('Hola')
+        .then(string => {
+            expect(string).toBe('aloH')
+        })
+})
+
+test('async await',async()=>{
+    const string = await reverseString2('hola')
+    expect(string).toBe('aloh')
+})
+
+afterEach(()=> console.log('Despues de cada prueba'))
+afterAll(()=>console.log('Despues de todas las pruebas'))
+beforeEach(()=>console.log('Antes de cada prueba'))
+beforeAll(()=>console.log('Antes de todas las pruebas'))
+
